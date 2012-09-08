3aa69c5378a74c051482d13bf4012e4e0530e5b5
Hung Dinh <hdneto17@gmail.com>
2012-09-08 18:33:15 -0400

    state.time

--- runrunlib/footballsimulationgamestate.py
+++ runrunlib/footballsimulationgamestate.py
@@ -1,9 +1,20 @@
 class FootballSimulationGameState(object):
-    def __init__(self, time=0):
+    def __init__(self, time=0, quarter=1):
         self._time = time
+        self._quarter = quarter
 
     def time(self, time):
-        return FootballSimulationGameState(time=time)
+        return FootballSimulationGameState(time=time,
+                                           quarter=self._quarter)
+
+    def quarter(self, quarter):
+        if quarter <= 0 or quarter >= 5:
+            raise RuntimeError('quarter must be between 1 and 4')
+        return FootballSimulationGameState(time=self._time,
+                                           quarter=quarter)
 
     def get_time(self):
         return self._time
+
+    def get_quarter(self):
+        return self._quarter
--- runrunlib/tests/test_footballsimulationgamestate.py
+++ runrunlib/tests/test_footballsimulationgamestate.py
@@ -3,6 +3,21 @@ from runrunlib.footballsimulationgamestate import FootballSimulationGameState
 
 
 class FootballSimulationGameStateTests(TestCase):
+    def test_get_time_at_constructor_should_be_zero(self):
+        # Arrange
+        state = state = FootballSimulationGameState()
+
+        # Assert
+        self.assertEqual(state.get_time(), 0)
+
+    def test_get_quarter_at_constructor_should_be_zero(self):
+        # Arrange
+        state = state = FootballSimulationGameState()
+
+        # Assert
+        self.assertEqual(state.get_quarter(), 1)
+
+
     def test_should_set_time(self):
         # Arrange & Act
         state = FootballSimulationGameState() \
@@ -10,3 +25,20 @@ class FootballSimulationGameStateTests(TestCase):
 
         # Assert
         self.assertEqual(state.get_time(), 138)
+
+    def test_should_set_quarter(self):
+        # Arrange & Act
+        state = FootballSimulationGameState() \
+                    .quarter(3)
+
+        # Assert
+        self.assertEqual(state.get_quarter(), 3)
+
+    def test_set_quarter_should_throw_error_if_out_of_range(self):
+        # Arrange & Act
+        state = FootballSimulationGameState()
+
+        # Assert
+        self.assertRaises(RuntimeError, state.quarter, -1)
+        self.assertRaises(RuntimeError, state.quarter, 0)
+        self.assertRaises(RuntimeError, state.quarter, 5)
