a5a7ba5b96d6638af8aedcb0309a3b41064f73af
Hung Dinh <hdneto17@gmail.com>
2012-09-08 18:08:34 -0400

    starting to go down simulation internals route (which contains football logic)

---
+++ runrunlib/footballsimulationinternals.py
@@ -0,0 +1,2 @@
+def process_state(state):
+    pass
--- runrunlib/tests/test_footballsimulation.py
+++ runrunlib/tests/test_footballsimulation.py
@@ -1,11 +1,17 @@
-from unittest import TestCase
+from unittest import TestCase, skip
 from runrunlib.footballsimulation import FootballGameSimulation
 from runrunlib import FootballGame, FootballTeam
 
 
 class FootballGameSimulationTests(TestCase):
+    @skip("will do gameinternals first")
     def test_shit(self):
         # Arrange
         team1 = FootballTeam('team1')
         team2 = FootballTeam('team2')
         simulation = FootballGameSimulation(team1, team2)
+        
+        # Act
+        game_result = simulation.sim_game()
+
+        # Assert
